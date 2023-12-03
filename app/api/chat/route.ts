94b7f8e653f4e5f5d1ddbac14801c5eec8a9db9c
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-12-03 04:08:21 +0000

    fix: route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -27,7 +27,7 @@ export async function POST(req: Request) {
             : null
         }
           ${article}
-          ${article.slice(-1) === "."}`,
+          ${article.slice(-1) === "." ? "" : "."}`,
       },
     ],
   });
