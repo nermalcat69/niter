f4b165722cade8378c05599befe452cb6e8fb43e
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-12-03 05:44:13 +0000

    feat: added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className="antialiased bodyfr">
         {children}
         <Analytics />
